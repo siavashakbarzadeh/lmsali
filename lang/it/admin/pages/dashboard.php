07fece03cb552ae537af17811fdaeb7471bd3add
siavashakbarzadeh1 <91117383+siavashakbarzadeh1@users.noreply.github.com>
2024-02-14 11:09:20 +0100

    j

--- lang/it/admin/pages/dashboard.php
+++ lang/it/admin/pages/dashboard.php
@@ -7,7 +7,6 @@ return [
     | Admin Dashboard Translation
     |--------------------------------------------------------------------------
     */
-
     'dashboard' => 'Dashboard',
-    'admin_dashboard_show' => 'Dashboard Show',
+    'admin_dashboard_show' => 'Mostra Dashboard',
 ];
